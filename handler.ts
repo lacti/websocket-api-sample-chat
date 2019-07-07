e4814c3cc78f57d1826dc47269e2a1334edcdf96
jaeyoung.choi <lactrious@gmail.com>
2019-07-07 18:08:59 +0900

    Initial commit

---
+++ handler.ts
@@ -0,0 +1,59 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { ApiGatewayManagementApi, DynamoDB } from "aws-sdk";
+import "source-map-support/register";
+
+export const connect: APIGatewayProxyHandler = async event => {
+  await new DynamoDB()
+    .putItem({
+      TableName: `ConnectionIds`,
+      Item: {
+        connectionId: { S: event.requestContext.connectionId }
+      }
+    })
+    .promise();
+  return {
+    statusCode: 200,
+    body: "OK"
+  };
+};
+
+export const disconnect: APIGatewayProxyHandler = async event => {
+  await new DynamoDB()
+    .deleteItem({
+      TableName: `ConnectionIds`,
+      Key: {
+        connectionId: { S: event.requestContext.connectionId }
+      }
+    })
+    .promise();
+  return {
+    statusCode: 200,
+    body: "OK"
+  };
+};
+
+export const broadcast: APIGatewayProxyHandler = async event => {
+  const dbResult = await new DynamoDB()
+    .scan({
+      TableName: `ConnectionIds`,
+      ProjectionExpression: "connectionId"
+    })
+    .promise();
+  const api = new ApiGatewayManagementApi({
+    endpoint: event.requestContext.domainName + "/" + event.requestContext.stage
+  });
+  await Promise.all(
+    dbResult.Items.map(async ({ connectionId }) =>
+      api
+        .postToConnection({
+          ConnectionId: connectionId.S,
+          Data: event.body
+        })
+        .promise()
+    )
+  );
+  return {
+    statusCode: 200,
+    body: "OK"
+  };
+};
